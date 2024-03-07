9090884477f5f4cdf7095320ac9900376dc527e6
TheLoneHobbyist <lonehobbyist@gmail.com>
2024-03-07 10:24:58 -0500

    Updated File Base
    Modified code to make functional
    
    Added a changable input method functionality, and random color functionality.

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
-document.addEventListener("DOMContentLoaded", function() {
-    createBoard(32)
-    console.log("hello")
-});
+function makeGrid(size, inMethod) {
+    if(size < 100) {
+        let board = document.getElementById('board');
+        for(i = 0; i < size; i++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            row.style.backgroundColor = 'white';
+            row.style.display = 'flex';
+            row.style.flexDirection = 'column';
+            row.style.gap = '1px';
+            row.style.flex = '1';
+            for(j = 0; j < size; j++) {
+                let column = document.createElement('div');
+                column.classList.add('column');
+                row.appendChild(column);
+                column.style.backgroundColor = 'white';
+                if(inMethod === "mousedown"){
+                    column.addEventListener('mousedown',function(){
+                        column.style.backgroundColor = 'black';
+                    })
+                }else if (inMethod === "mouseover") {
+                    column.addEventListener("mouseover",function(){
+                        column.style.backgroundColor = randColor();
+                    })
+                }
+                column.style.flex = '1';
+                column.style.boder = '1px solid gray';
+            }
+            board.appendChild(row);
+        }
+    }else {alert("Choose a value less than 100 please")}
+}
 
-function createBoard(size) {
-    let board = document.querySelector(".board");
-
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    let numDivs = size * size;
-
-    for(let i = 0; i < numDivs; i++) {
-        let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend", div);
-        
+function clearBoard() {
+    let board = document.getElementById('board');
+    while (board.hasChildNodes()){
+        board.removeChild(board.firstChild);
     }
+}
+function getSize(inMethod) {
+    clearBoard();
+    let size =+ prompt("What size would you like the board to be. Chose between 1 and 99");
+    makeGrid(size,inMethod);
+}
+
+
+function getInputMethod() {
+    let inputMethod = prompt("Would you like to click or hover?");
+    let inMethod = inputMethod;
+    switch(inMethod) {
+        case "click":
+            inMethod = 'mousedown';
+            getSize(inMethod);
+            break;
+        case "hover":
+            inMethod = 'mouseover';
+            getSize(inMethod);
+            break;
+        default:
+            inMethod = 'mouseover';
+            break;
+    }
+    
+}
+
+
+function randColor(){
+    let colors = ["black","blue","gray","pink","brown","green","purple","red"];
+    while(true){
+        let randomColor = colors[Math.floor(Math.random()*colors.length)];
+        return randomColor;
+    }
+    
+
 }
